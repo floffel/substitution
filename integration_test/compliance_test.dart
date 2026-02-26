af6cf3fdd46e3e431350ecd4690286b2bc389859
Florian Minnecker <florian@minnecker.com>
2026-02-26 13:23:22 +0100

    fix: remove unnecessary imports to satisfy strict CI analysis

--- integration_test/compliance_test.dart
+++ integration_test/compliance_test.dart
@@ -1,5 +1,4 @@
 import 'dart:io' as dart_io;
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:substitution/main.dart' as app;
